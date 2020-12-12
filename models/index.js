cbc05feaec49072f138102ed4465724f33ea1bd5
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-11 19:48:05 -0500

    Add configs and Export sequelize operators

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize')
 const allConfigs = require('../config/sequelize')
 const AuthorsModel = require('./authors')
+
 const GenresModel = require('./genres')
 const NovelsModel = require('./novels')
 const NovelsGenresModal = require('./novelsGenres')
-
 
 const environment = process.env.NODE_ENV || 'development'
 const config = allConfigs[environment]
@@ -12,9 +12,6 @@ const config = allConfigs[environment]
 const connection = new Sequelize(config.database, config.username, config.password, {
   host: config.host, dialect: config.dialect
 })
-
-
-
 
 const Novels = NovelsModel(connection, Sequelize)
 const Authors = AuthorsModel(connection, Sequelize, Novels)
@@ -24,7 +21,13 @@ const NovelsGenres = NovelsGenresModal(connection, Sequelize, Novels, Genres)
 Authors.hasMany(Novels)
 Novels.belongsTo(Authors)
 
+
 Genres.belongsToMany(Novels, { through: NovelsGenres })
 Novels.belongsToMany(Genres, { through: NovelsGenres })
 
-module.exports = { Authors, Genres, Novels, NovelsGenres }
+
+
+
+module.exports = {
+  Authors, Genres, Novels, NovelsGenres, Op: Sequelize.Op,
+}
