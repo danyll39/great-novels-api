f2b177f41b20fa4f7596e6c92dad00c589c370c0
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-12-11 20:00:52 -0500

    Add like Operator and wildcard to to fuzzy match last name

--- controllers/authors.js
+++ controllers/authors.js
@@ -2,26 +2,41 @@ const models = require('../models')
 
 const getAllAuthors = async (request, response) => {
   const authors = await models.Authors.findAll({
+
   })
 
   return response.send(authors)
 }
+
 const getAuthorById = async (request, response) => {
   try {
-    const { id } = request.params
+    const { identifier } = request.params
+
     const foundAuthor = await models.Authors.findOne({
-      where: { id },
-      include: [
-        { model: models.Novels, include: [{ model: models.Genres }] }
-      ]
+
+      include: [{
+        model: models.Novels,
+        include: [{ model: models.Genres }]
+      }],
+      where: {
+        [models.Op.or]: [
+          { id: identifier },
+          { nameLast: { [models.Op.like]: `%${identifier}%` } },
+        ],
+
+      }
     })
 
     return foundAuthor
+
+
       ? response.send(foundAuthor)
       : response.sendStatus(404)
   } catch (error) {
     return response.status(500).send('Unable to retrieve author, please try again')
   }
 }
+
+
 
 module.exports = { getAllAuthors, getAuthorById }
